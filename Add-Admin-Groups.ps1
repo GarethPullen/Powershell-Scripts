e8c75083fa3baec11b1e1ed8bd502947870ec4eb
Gareth Pullen <66172464+GarethPullen@users.noreply.github.com>
2022-03-28 09:18:52 +0100

    Update Add-Admin-Groups.ps1

--- Add-Admin-Groups.ps1
+++ Add-Admin-Groups.ps1
@@ -2,7 +2,7 @@
 #Is uploaded to Intune > Scripts to run on all Autopilot devices.
 #Updated 04/10/2021 to check if the groups alread exist, to avoid it failing.
 #Updated 02/2022 to remove identifying info prior to upload
-$SupportTeam = $true
+$IntuneUsers = $true
 $CSCSAdminRightsClientPcs = $true
 
 foreach ($group in Get-LocalGroup -Name "Administrators") {
@@ -12,14 +12,14 @@ foreach ($group in Get-LocalGroup -Name "Administrators") {
 foreach ($group in $group_members) {
     if ($group -match "S-1-12-1-<Sanitized>") {
         #The group above is the SID of the Intune Users group.
-        $SupportTeam = $false  
+        $IntuneUsers = $false  
         #If the group is matched, make this variable False as we don't need to add it again.
     }
     if ($group -match "S-1-12-1-<Sanitized>") {
         $CSCSAdminRightsClientPcs = $false
     }
 }
-If ($SupportTeam -eq $true) {
+If ($IntuneUsers -eq $true) {
     Add-LocalGroupMember -Group Administrators -Member "S-1-12-1-<Sanitized>"
 }
 If ($CSCSAdminRightsClientPcs -eq $true) {
