a351d86910a3c4c34437722ef5b82ac18a633bef
Gareth Pullen <66172464+GarethPullen@users.noreply.github.com>
2023-08-07 14:25:56 +0100

    Update Long paths or files.ps1
    Commented out Truncate parts, moved logic for it to the end, added a Switch for it. Improved Verbose logging.

--- Long paths or files.ps1
+++ Long paths or files.ps1
@@ -2,19 +2,25 @@
     Will also list any which are over 260 characters long (combined file & folder path length)
     Written 24/05/2023 by Gareth Pullen (grp43)
     Updated 25/05/2023 to have additional "Write-Verbose" for troubleshooting
+    Updated 07/06/2023 to truncate file & folder names
+    Updated 02/08/2023 to comment out the auto-truncate
+    Updated 07/08/2023 to improve the Verbose output & handling of access-denied errors. Moved logic for truncate to the end & added a Switch for it.
 #>
 
 [CmdletBinding()]
 Param(
     [switch] $Silent,
     [Switch] $Help,
+    [switch] $truncate,
     [Switch] $display
 )
 #Switches to allow for "-Silent" or "-Help" to be called
 If ($Help.IsPresent) {
     #Help was called!
-    Write-Host "This script asks you for a folder to write CSV File(s) to - one for long-paths, one for long file-names, and one for total-length is 260 or more."
+    Write-Host 'Unless called with the "-display" parameter'
+    Write-Host "This script asks you for a folder to write CSV File(s) to - one for long-paths, one for long file-names, and one for total-length longer than specified."
     Write-Host 'It will call them "long-paths.csv" "long-files.csv" and "Long-Combined-Length.csv"'
+    Write-Host 'If called with the "-display" switch it will not write to CSV and will instead just write to the console'
     Write-Host 'It supports the switches "-Silent" to suppress most messages, "-Verbose" to show more messages, "-display" to only output to the console, and "-Help" to show this'
     Exit
 }
@@ -52,55 +58,96 @@ Do {
 #Change it to avoid the 260-character limit
 if ($CheckPath.Substring(0, 2) -eq "\\") {
     #Has leading "\\"
-    $CheckPath = $CheckPath -replace '^\\\\', '\\?\UNC\'
     Write-Verbose -Message "Changing UNC to Unicode UNC"
+    $CheckPath = $CheckPath -replace '^\\\\', '\\?\UNC\'
 }
 Else {
     #No leading "\\" - so just add the \\?\
-    $CheckPath = '\\?\' + $CheckPath
     Write-Verbose -Message "Changing path to unicode type"
+    $CheckPath = '\\?\' + $CheckPath
 }
 
 #Ask for the folder length to look for - default to 200 otherwise
 $AskUserLength = $null
-$AskUserLength = Read-Host 'Enter a maximum folder length to check (defaults to 200 if nothing is entered)'
+$AskUserLength = Read-Host 'Enter a maximum folder full-path length to check (defaults to 200 if nothing is entered)'
+Write-Verbose "Checking if a length has been specified"
+If ($AskUserLength -gt 1) {
+    $FolderFullPathLength = $AskUserLength
+}
+Else {
+    $FolderFullPathLength = 200
+}
+$AskUserLength = $null
+$AskUserLength = Read-Host 'Enter a maximum folder length to check (i.e. maximum length for a sigle-folder. Defaults to 50 characters)'
 Write-Verbose "Checking if a length has been specified"
 If ($AskUserLength -gt 1) {
     $FolderLength = $AskUserLength
 }
 Else {
-    $FolderLength = 200
+    $FolderLength = 50
 }
-$AskUserFileLength = $null
-$AskUserfileLength = Read-Host 'And for maximum file-length? (defaults to 100 if nothing is entered)'
+$AskUserLength = $null
+$AskUserLength = Read-Host 'Enter a maximum File length to check (i.e. maximum length for a file-name. Defaults to 50 characters)'
 Write-Verbose "Checking if a length has been specified"
-If ($AskUserFileLength -gt 1) {
-    $FileLength = $AskUserFileLength
+If ($AskUserLength -gt 1) {
+    $FileLength = $AskUserLength
 }
 Else {
-    $FileLength = 100
+    $FileLength = 50
 }
+
 
 # Enumerate the subfolders
 Write-Verbose "Getting list of directories"
-$FolderList = Get-ChildItem -Path $CheckPath -Directory -Recurse
+if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+    $Errors = New-Object System.Collections.Generic.List[System.Object]
+    #Verbose specified, so we want to show the errors when checking folder lists:
+    Write-Verbose "Verbose specified, will list any problem folders"
+    Try {
+        $FolderList = Get-ChildItem -Path $CheckPath -Directory -Recurse -EA stop    
+    }
+    Catch {
+        $Errors.Add($PSItem.Exception.Message)
+    }
+    If ($Errors.Count -gt 0) {
+        #Errors has something in it
+        Write-Verbose "The following errors were encountered while getting the folder-list:"
+        foreach ($Message in $Errors) {
+            Write-Host -ForegroundColor red $Message
+        }
+    }
+    # Clear the $Errors variable:
+    Clear-Variable -Name "Errors"
+}
+Else {
+    #Verbose not specified, silently continue on errors to suppress them.
+    $FolderList = Get-ChildItem -Path $CheckPath -Directory -Recurse -EA Silentlycontinue
+}
+
+
 #check the length of the folders
-$LongFolders = New-Object System.Collections.Generic.List[System.Object]
+$LongFolderFullPath = New-Object System.Collections.Generic.List[System.Object]
+$LongFolderPath = New-Object System.Collections.Generic.List[System.Object]
 $CSVWritten = New-Object System.Collections.Generic.List[System.Object]
 Write-Verbose "Will now loop through list of folders checking the length"
 Foreach ($Folder in $FolderList) {
     #-4 to account for the extra "\\?\" we added.
-    if ((($Folder.FullName).Length - 4) -gt $FolderLength) {
-        $LongFolders.Add($Folder)
+    if ((($Folder.FullName).Length - 4) -ge $FolderFullPathLength) {
+        $LongFolderFullPath.Add($Folder)
+    }
+    if ((($Folder.Name)).Length -ge $FolderLength) {
+        #If the folder-name is long then add it to a list
+        $LongFolderPath.Add($Folder)
     }
 }
-If ($LongFolders.Count -gt 0) {
-    Write-Verbose "Folders longer than $FolderLength found - either displaying or writing them out"
+
+If ($LongFolderFullPath.Count -gt 0) {
+    Write-Verbose "Folders longer than $FolderFullPathLength found - either displaying or writing them out"
     #There's something in the list!
     if ($PSBoundParameters.ContainsKey('display')) {
         #We only want to display the results
-        Write-Host "The following folders are longer than $FolderLength"
-        foreach ($Entry in $LongFolders) {
+        Write-Host "The following folders are longer than $FolderFullPathLength"
+        foreach ($Entry in $LongFolderFullPath) {
             $Entry = $Entry -replace '^\\\\\?\\', ''
             Write-Host $Entry
         }
@@ -109,15 +156,35 @@ If ($LongFolders.Count -gt 0) {
         #Display was not set, so output to CSV.
         $ExportFolderPath = $ExportPath + "Long-Folders.csv"
         Write-Verbose "Attempting to write list of long-folders to $ExportFolderPath"
-        $LongFolders | Select FullName | Export-Csv -NoTypeInformation -Path $ExportFolderPath 
+        $LongFolderFullPath | Select FullName | Export-Csv -NoTypeInformation -Path $ExportFolderPath 
         $CSVWritten.Add($ExportFolderPath)
     }
 }
 
 # Enumerate the subfiles
 Write-Verbose "Getting list of files"
-$FileList = Get-ChildItem -Path $CheckPath -Recurse
-
+if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+    $Errors = New-Object System.Collections.Generic.List[System.Object]
+    #Verbose specified, so we want to show the errors when checking folder lists:
+    Write-Verbose "Verbose specified, will list any problem folders"
+    Try {
+        $FileList = Get-ChildItem -Path $CheckPath -File -Recurse -Force -ErrorAction stop    
+    }
+    Catch {
+        $Errors.Add($PSItem.Exception.Message)
+    }
+    If ($Errors.Count -gt 0) {
+        #Errors has something in it
+        Write-Verbose "The following errors were encountered while getting the file-list:"
+        foreach ($Message in $Errors) {
+            Write-Host -ForegroundColor red $Message
+        }
+    }
+}
+Else {
+    #Verbose not specified, silently continue on errors to suppress them.
+    $FileList = Get-ChildItem -Path $CheckPath -File -Recurse -Force -ErrorAction SilentlyContinue
+}
 #Create List objects for the file lists
 $LongFiles = New-Object System.Collections.Generic.List[System.Object]
 $LongTotalPath = New-Object System.Collections.Generic.List[System.Object]
@@ -125,10 +192,10 @@ $LongTotalPath = New-Object System.Collections.Generic.List[System.Object]
 #check the length of the files
 Write-Verbose "About to loop through file-list for those over $FileLength or 260"
 Foreach ($File in $FileList) {
-    if (($File.Name).Length -gt $FileLength) {
+    if (($File.Name).Length -ge $FileLength) {
         $LongFiles.Add($File)
     }
-    if ($File.Fullname.Length -gt 264) {
+    if ($File.Fullname.Length -ge 264) {
         #We definitely want to log "Path Longer than 264! (260 + 4 for the "\\?\")"
         $LongTotalPath.Add($File)
     }
@@ -178,3 +245,83 @@ if (!($PSBoundParameters.ContainsKey('display'))) {
         Write-Host $FileWritten
     }
 }
+
+<#
+if ($PSBoundParameters.ContainsKey('truncate')) {
+    #Check for file-length trimming options!
+    $AskUserLength = $null
+    $AskUserLength = Read-Host 'What should I trim the folder-name down to? (Defaults to 15 characters if nothing is set)'
+    Write-Verbose "Checking if a length has been specified"
+    If ($AskUserLength -gt 1) {
+        $FolderTrimLength = $AskUserLength
+    }
+    Else {
+        $FolderTrimLength = 15
+    }
+
+    $AskUserFileLength = $null
+    $AskUserfileLength = Read-Host 'To what length should long-files be trimmed to? (Defaults to 20 characters if nothing is entered)'
+    Write-Verbose "Checking if a length has been specified"
+    If ($AskUserFileLength -gt 1) {
+        $FileTrimLength = $AskUserFileLength
+    }
+    Else {
+        $FileTrimLength = 20
+    }
+    #>
+
+    <#
+    ### Now we check if the user really wants us to rename the files & folders...
+    $TotalLongFolders = $LongFolderPath.Count
+    $TotalLongFilenames = $LongFiles.Count
+    If ($TotalLongFolders -gt 0) {
+        Write-Output "We found $TotalLongFolders folders longer than $FolderLength"
+    }
+    If ($TotalLongFilenames -gt 0) {
+        Write-Output "We found $TotalLongFilenames files longer than $FileLength"
+    }
+    If ($TotalLongFolders + $TotalLongFilenames -eq 0) {
+        Write-Output "Nothing to truncate, quitting"
+        Exit
+    }
+    Write-Output "Do you really want to truncate all of those files and folders? Check the CSV files listed above for all which will be renamed"
+    Write-Host 'Please enter "Y" to confirm you want to rename them all!'
+    $UserConfirmed = "N"
+    $UserConfirmed = Read-Host -Prompt "Really truncate? This CANNOT be undone! (y/N)"
+    If ($UserConfirmed.ToUpper() -eq "Y") {
+        $RenamedItemsCSV = $ExportPath + "RenamedItems.csv"
+        #Do the renaming...
+        $RenamedItems = @{}
+        Foreach ($Folder in $LongFolderPath) {
+            Try {
+                $NewFolderName = ($Folder.Name.Substring(0, $FolderLength)).Trim()
+                $RenamedItems.Add($Folder.FullName, $NewFolderName)
+                Write-Verbose "Renaming $Folder to $NewFolderName"
+                Rename-Item -Path $Folder -NewName $NewFolderName
+            }
+            Catch {
+                Write-Error "Error renaming $Folder!"
+            }
+        }
+        Foreach ($File in $LongFiles) {
+            Try {
+                $Extension = $File.Extension
+                $NewFileName = ($File.Name.Substring(0, $FileLength)).Trim() + $Extension
+                $RenamedItems.Add($File.FullName, $NewFileName)
+                Write-Verbose "Renaming $File to $NewFileName"
+                Rename-Item -Path $File -NewName $NewFileName
+            }
+            Catch {
+                Write-Error "Error renaming $File!"
+            }
+        }
+
+        #Write the list of renamed items to a CSV and tell the user that we've written a CSV of renamed items
+        $RenamedItems.GetEnumerator() | Select-Object -Property @{N = 'Old-Name'; E = { $_.Key } }, @{N = 'New-Name'; E = { $_.Value } } | Export-Csv -NoTypeInformation -Path $RenamedItemsCSV
+        Write-Output "Renamed items have been logged to: $RenamedItemsCSV"
+    }
+    Else {
+        Write-Host "You decided not to rename them. Check the CSV lists for long files."
+    }
+}
+#>
