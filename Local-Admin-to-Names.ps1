6032237d0a9b3c569cb1ef5d5007162a02214c6c
Gareth Pullen <66172464+GarethPullen@users.noreply.github.com>
2024-05-21 12:45:50 +0100

    Update Local-Admin-to-Names.ps1
    Modified to install AzureAD as "CurrentUser" if not running as Admin.

--- Local-Admin-to-Names.ps1
+++ Local-Admin-to-Names.ps1
@@ -1,9 +1,11 @@
 <# Script to get local Administrators groups and list the AzureAD group names from the SIDs
 Written 30/03/2021 by Gareth Pullen (grp43)
+Edited to not query for login if an Azure user is signed in
+#Modified 21/05/2024 to try installing AzureAD scoped to Current User if not running as admin
 #>
 
 function Convert-AzureAdSidToObjectId {
-<#
+    <#
 .SYNOPSIS
 Convert a Azure AD SID to Object ID
  
@@ -28,26 +30,45 @@ The SID to convert
     return $guid
 }
 
+#Check if we're running as admin - if not, install module as "Current User" scope
+If (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    #True if running as admin, false if not
+    $Admin = $true
+}
+Else {
+    $Admin = $false
+}
+
 #Check if AzureAD Module is installed, install it if not.
 try {
-    get-installedmodule -name azuread -ErrorAction Stop |out-null
+    get-installedmodule -name azuread -ErrorAction Stop | out-null
 }
 Catch {
     If ((get-psrepository -Name 'PSGallery').Trusted) {
-        install-module -name AzureAD
-    } Else {    
-        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-        install-module -name AzureAD
-        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+        If ($Admin) {
+            install-module -name AzureAD
+        }
+        Else {
+            install-module -Scope CurrentUser -Name AzureAD
+        }
+    }
+    Else {
+        if ($Admin) {
+            Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+            install-module -name AzureAD
+            Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+        }
+        Else {
+            Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+            install-module -Scope CurrentUser -name AzureAD
+            Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+        }
     }
 }
-
-#Get the current user UPN - may avoid login prompt
 $UPN = whoami /upn
 #Connect to AzureAD (Prompts for login)
 Try {
     if ($UPN -match "@med*") {
-    #UPN is an AAD one, try to login with that.
         Connect-AzureAD -AccountId $UPN -EA SilentlyContinue | out-null
     }
     Else {
@@ -62,17 +83,24 @@ Catch {
 
 foreach ($group in Get-LocalGroup -Name "Administrators") {
     $group = [ADSI]"WinNT://$env:COMPUTERNAME/$group"
-    $group_members = @($group.Invoke('Members') | % {([adsi]$_).path}) -Split "WinNT://"
-    }
-$ADIDs = @()
-foreach ($group in $group_members){
-    if ($group -match "S-1-12-1"){
-        $ADIDs += $group + " - " + (Get-AzureADObjectByObjectId -ObjectIds (Convert-AzureAdSidToObjectId($group))).DisplayName
-    }  
+    $group_members = @($group.Invoke('Members') | % { ([adsi]$_).path }) -Split "WinNT://"
 }
-if ($ADIDs -ne $Null){
-Write-Host "AAD Groups: "
-foreach ($ID in $ADIDs){
-    Write-Host $ID
+$ADIDs = @()
+foreach ($group in $group_members) {
+    if ($group -match "S-1-12-1") {
+        $ADIDs += (Get-AzureADObjectByObjectId -ObjectIds (Convert-AzureAdSidToObjectId($group))).DisplayName
     }
+    elseif ($group -ne "") {
+        $ADIDs += $group
+    }
+}
+
+if ($ADIDs -ne $Null) {
+    Write-Host "AAD Groups: "
+    foreach ($ID in $ADIDs) {
+        Write-Host $ID
+    }
+}
+Else {
+    Write-host "No AzureAD groups found"
 }
